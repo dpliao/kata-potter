1bf197afab9cf2df9a610152210abdd0e1b981b6
dpliao <dunpeiliao@gmail.com>
2022-05-15 23:35:20 +0800

    first red

--- libs/potter.py
+++ libs/potter.py
@@ -2,3 +2,8 @@ class Potter:
     
     def __init__(self):
         self.books = []
+        
+    def get_price(self, book_list):
+        self.books = book_list
+        total = 0
+        return total
