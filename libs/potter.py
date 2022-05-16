eec97a801d87be6eeedd5263ea490063965210db
dpliao <dunpeiliao@gmail.com>
2022-05-16 15:37:15 +0800

    refactor

--- libs/potter.py
+++ libs/potter.py
@@ -5,12 +5,7 @@ DISCOUNT = {1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
     
 def get_price(books):
     set_len = divide_into_sets(books)
-    lengths = Counter(set_len)
-    while lengths[3] > 0 and lengths[5] > 0:
-        lengths[3] -= 1
-        lengths[5] -= 1
-        lengths[4] += 2
-    best_sets =  lengths.elements()
+    best_sets = replace_sets(set_len)
     return get_discount(best_sets)
 
 def divide_into_sets(books):
@@ -21,6 +16,14 @@ def divide_into_sets(books):
         for u in unique_set:
             books.remove(u)
     return set_len
+
+def replace_sets(sets):
+    lengths = Counter(sets)
+    while lengths[3] > 0 and lengths[5] > 0:
+        lengths[3] -= 1
+        lengths[5] -= 1
+        lengths[4] += 2
+    return lengths.elements()
 
 def get_discount(sets):
     return sum(BOOK_PRICE * length * DISCOUNT[length] for length in sets)
