c25de74f952d757d8eebf2aa338e0ccd02d42ed2
dpliao <dunpeiliao@gmail.com>
2022-05-16 15:08:13 +0800

    refactor

--- libs/potter.py
+++ libs/potter.py
@@ -1,22 +1,18 @@
-
-from enum import unique
-
-
-class Potter:
-    BOOK_PRICE = 8
-    DISCOUNT = {1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
+BOOK_PRICE = 8
+DISCOUNT = {1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
     
-    def __init__(self):
-        self.books_list = []
-        
-    def get_price(self, books):
-        self.books_list = books
-        
-        self.set_len = []
-        while books:
-            unique_set = set(books)
-            self.set_len.append(len(unique_set))
-            for u in unique_set:
-                books.remove(u)
-            
-        return sum(self.BOOK_PRICE * length * self.DISCOUNT[length] for length in self.set_len)
+def get_price(books):
+    set_len = divide_into_sets(books)
+    return sum(get_discount(length) for length in set_len)
+
+def divide_into_sets(books):
+    set_len = []
+    while books:
+        unique_set = set(books)
+        set_len.append(len(unique_set))
+        for u in unique_set:
+            books.remove(u)
+    return set_len
+
+def get_discount(length):
+    return BOOK_PRICE * length * DISCOUNT[length]
--- tests/test_potter.py
+++ tests/test_potter.py
@@ -1,17 +1,22 @@
 import pytest
-from libs.potter import Potter
+from libs.potter import get_price
 
-potter = Potter()
 def testBasics():
-  assert 0 == potter.get_price([])
-  assert 8 == potter.get_price([1])
-  assert 8 == potter.get_price([2])
-  assert 8 == potter.get_price([3])
-  assert 8 == potter.get_price([4])
-  assert 8 * 3 == potter.get_price([1, 1, 1])
+  assert 0 == get_price([])
+  assert 8 == get_price([1])
+  assert 8 == get_price([2])
+  assert 8 == get_price([3])
+  assert 8 == get_price([4])
+  assert 8 * 3 == get_price([1, 1, 1])
 
 def testSimpleDiscounts():
-  assert 8 * 2 * 0.95 == potter.get_price([0, 1])
-  assert 8 * 3 * 0.9 == potter.get_price([0, 2, 4])
-  assert 8 * 4 * 0.8 == potter.get_price([0, 1, 2, 4])
-  assert 8 * 5 * 0.75 == potter.get_price([0, 1, 2, 3, 4])
+  assert 8 * 2 * 0.95 == get_price([0, 1])
+  assert 8 * 3 * 0.9 == get_price([0, 2, 4])
+  assert 8 * 4 * 0.8 == get_price([0, 1, 2, 4])
+  assert 8 * 5 * 0.75 == get_price([0, 1, 2, 3, 4])
+
+def testSeveralDiscounts():
+  assert 8 + (8 * 2 * 0.95) == get_price([0, 0, 1]) 
+  assert 2 * (8 * 2 * 0.95) == get_price([0, 0, 1, 1]) 
+  assert (8 * 4 * 0.8) + (8 * 2 * 0.95) == get_price([0, 0, 1, 2, 2, 3])
+  assert 8 + (8 * 5 * 0.75) == get_price([0, 1, 1, 2, 3, 4])
