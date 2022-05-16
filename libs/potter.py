c3c44116012b347ce42242fa55e890a8caa9ec51
dpliao <dunpeiliao@gmail.com>
2022-05-16 15:36:38 +0800

    third green

--- libs/potter.py
+++ libs/potter.py
@@ -1,9 +1,17 @@
+from typing import Counter
+
 BOOK_PRICE = 8
 DISCOUNT = {1:1, 2:0.95, 3:0.9, 4:0.8, 5:0.75}
     
 def get_price(books):
     set_len = divide_into_sets(books)
-    return sum(get_discount(length) for length in set_len)
+    lengths = Counter(set_len)
+    while lengths[3] > 0 and lengths[5] > 0:
+        lengths[3] -= 1
+        lengths[5] -= 1
+        lengths[4] += 2
+    best_sets =  lengths.elements()
+    return get_discount(best_sets)
 
 def divide_into_sets(books):
     set_len = []
@@ -14,5 +22,5 @@ def divide_into_sets(books):
             books.remove(u)
     return set_len
 
-def get_discount(length):
-    return BOOK_PRICE * length * DISCOUNT[length]
+def get_discount(sets):
+    return sum(BOOK_PRICE * length * DISCOUNT[length] for length in sets)
